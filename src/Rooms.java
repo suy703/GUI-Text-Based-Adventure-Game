e55bd4d2dd4e92ecd4a91d32ae53329ab8a79e57
= <=>
2017-10-25 23:30:20 -0400

    UPDATED Room: ADDED Inn & Items in DrugStore

--- src/Rooms.java
+++ src/Rooms.java
@@ -16,6 +16,7 @@ import javafx.scene.control.TextArea;
 public class Rooms {
 	CommandMenu commandMenu = new CommandMenu();
 	
+	
 	//GAME BACKGROUND----------------------------------------------------------------------------------
 	final Image gameBackground = new Image("file:images/background.jpg");
 	final BackgroundImage backgroundImage = new BackgroundImage(gameBackground, null, null, null, null);
@@ -31,7 +32,8 @@ public class Rooms {
 	Boolean lockMainMenu = true;
 	Boolean unlockSaveGame = false;
 	
-	Boolean bombayHall = true;
+	Boolean drugStore = true;
+	Boolean inn = true;
 	
 	public void mainMenu(Stage stage, Scene scene) {
 		
@@ -72,13 +74,13 @@ public class Rooms {
 				TownHub_1A();
 				lockMainMenu = false;
 				unlockSaveGame = true;
+				inn = false;
 			}
 			//Load Game
 			else if(command.equalsIgnoreCase("l") && lockMainMenu) {
 				displayStory = commandMenu.loadGameStory(displayStory, "Load Game");
 				displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Back (B)");
 				commandMenu.loadGame("testGame.ini");
-				
 			}
 			//Back
 			else if(command.equalsIgnoreCase("b") && lockMainMenu) {
@@ -101,8 +103,22 @@ public class Rooms {
 			}
 			
 			//DrugStore
-			if(command.equalsIgnoreCase("nw")) {
+			if(command.equalsIgnoreCase("nw") && drugStore) {
 				DrugStore_1B();
+				inn = true;
+			}
+			else if(command.equalsIgnoreCase("sr") && drugStore) {
+				Potion healthPotion = new Potion("i1", "Health Potion (HP)\n", "A bottle shaped like a human heart with a red liquid inside.", 2, "Potion", 3);
+				Potion attackPotion = new Potion("i2", "Attack Potion (AP)\n", "A perfectly round bottle with a cork in the top. The liquid has a strange yellow color to it but it smells like a delicious tropical fruit when you uncork it.", 3, "Potion", 2);
+				Weapon pistol = new Weapon("i7", "Pistol (P)\n", "A shiney, metalic, little gun. It might be small but it could definitely do some damage!", 5, 5);
+				displayStory = commandMenu.loadGameStory(displayStory, "Drug Store\n\n" + healthPotion.getName() + attackPotion.getName() + pistol.getName() + "\n -> Drug Store (NW)");
+				displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Examine Item (EI)\n" 
+						+ "-> Inventory (I)\n" + "-> Save Game (SG)");
+			}
+			
+			//Inn
+			if(command.equalsIgnoreCase("s") && inn) {
+				Inn_1C();
 			}
 		});
         /*
@@ -125,9 +141,9 @@ public class Rooms {
 		displayStory = commandMenu.loadGameStory(displayStory, "Town Hub\n\n" + "A dusty road leads into town and a tumbleweed "
 				+ "rolls across your path.  In the town square there is a statue of a menacing looking figure with a sheriff’s "
 				+ "badge on. The buildings look a bit run down and people seem to shuffle from place to place without interacting. "
-				+ "To the East there is a bustling saloon. To your North-West you see a drug store and to the South-West, an Inn.");
-		displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Drug Store (NW)\n" + "-> Inn (SW)\n" 
-				+ "-> Saloon (E)\n\n" + "-> Inventory (I)\n" + "-> Save Game (SG)");
+				+ "To the East there is a bustling saloon. To your North-West you see a drug store and to the South-West, an Inn.\n\n"
+				+ "-> Drug Store (NW)\n" + "-> Inn (SW)\n" + "-> Saloon (E)");
+		displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Inventory (I)\n" + "-> Save Game (SG)");
 	}
 	//Drug Store
 	public void DrugStore_1B() {
@@ -138,8 +154,22 @@ public class Rooms {
 				+ "top. Glass bottles full of medicine line the walls, but they look as if they haven’t been touched in years. You "
 				+ "could almost measure the amount of dust accumulation on them. A man with a large mustache and a tattered suit "
 				+ "stands behind the counter looking hopeful. To the South is a small passageway leading into a small, dimly lit, room "
-				+ "and to the East is the door leading to the Town Hub.");
-		displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Town Hub (E)\n" + "-> Inn (S)\n\n" 
+				+ "and to the East is the door leading to the Town Hub.\n\n" + "-> Town Hub (E)\n" + "-> Inn (S)");
+		displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Search Room (SR)\n" 
+				+ "-> Inventory (I)\n" + "-> Save Game (SG)");
+	}
+	//Inn
+	public void Inn_1C() {
+		
+		viewMap = commandMenu.navigateMap(map, viewMap);
+		viewIcon = commandMenu.navigateIcon(icon, viewIcon, 345, 334);
+		displayStory = commandMenu.loadGameStory(displayStory, "Inn\n\n" + "You walk into the inn and you see "
+				+ "a counter with a bell on it. There doesn’t appear to be anyone around, but you see a few "
+				+ "rooms with the doors closed. There are a few dusty painting hanging on the wall, they look "
+				+ "like they have seen better days. There is a hallway to the North that leads into a larger, "
+				+ "well lit room and to the East is the door to the Town Hub. The inn keeper comes up before "
+				+ "you get a chance to ring the bell and asks you if you want a room.\n\n" + "-> Drug Store (N)\n" + "-> Town Hub (E)");
+		displayCommand = commandMenu.loadGameCommand(displayCommand, "Action\n" + "-> Search Room (SR)\n" 
 				+ "-> Inventory (I)\n" + "-> Save Game (SG)");
 	}
 }
